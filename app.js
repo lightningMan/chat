ecb0ebd71f024b9211730ef1934f682845131921
hackjay <raychou8@gmail.com>
2013-05-15 13:20:34 +0800

    用websocket失败

--- app.js
+++ app.js
@@ -4,31 +4,31 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , test = require('./routes/test')
-  , code = require('./routes/code')
-  , http = require('http')
-  , path = require('path')
-  , db = require('./dao/codeDao')
-  , iLog = require('./tools/iLog');
+, routes = require('./routes')
+, test = require('./routes/test')
+, code = require('./routes/code')
+, http = require('http')
+, path = require('path')
+, db = require('./dao/codeDao')
+, iLog = require('./tools/iLog');
 
 var app =  module.exports = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 81);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', 3001 | process.env.PORT);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('product', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
@@ -45,14 +45,56 @@ db.connect(function(error){
 });
 app.on('close', function(errno) {
 	db.disconnect(function(err) {
-	  if (!err) {
-		console.log('数据库连接关闭!');
-	  }
+		if (!err) {
+			console.log('数据库连接关闭!');
+		}
 	});
 });
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
 });
 
+
+//chat相关代码
+var WebSocketServer = require('ws').Server;
+console.log(require('util').inspect(WebSocketServer));
+var wss = new WebSocketServer({server : server});
+var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+var clients = [];
+
+wss.on('connection', function(ws){
+	clients.push(ws);
+	var userName = false;
+	var userColor = false;
+	ws.on('message', function(msg){
+		if(!userName){
+			userName = msg;
+			userColor = colors.shift();
+			ws.send(JSON.stringify({ type:'color', data: userColor }));
+			console.log(userName + ' login');
+		}else{
+			console.log(userName + ' say: ' + msg);
+			var obj = {
+				time: (new Date()).getTime(),
+		text: msg,
+		author: userName,
+		color: userColor
+			};
+			var json = JSON.stringify({type:'message', data: obj});
+			for (var i=0; i < clients.length; i++) {
+				console.log(clients.length);
+				clients[i].send(json);
+			}
+		}
+	});
+	ws.on('close', function(){
+		if(userName !== false && userColor != false){
+			var index = clients.indexOf(ws);
+			clients.splice(index, 1);
+			colors.push(userColor);
+		}  
+	});
+});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 $(function() {
-	var cpp_src = '#include <stdio.h>\n#include <iostream>\nusing namespace std;\n\nint main()\n{\n  cout << "hello world" << endl;\n  \n}\n'
+	var cpp_src = '#include <cstdio>\n#include <iostream>\nusing namespace std;\n\nint main()\n{\n  cout << "hello world" << endl;\n  return 0;\n}\n'
 	require.config({
 		paths: {
 			ace: "lib/ace"
@@ -28,13 +28,13 @@ function share() {
 	var inData = input.val();
 	var data = {
 		src: src,
-input: inData
+		input: inData
 	};
 	$.post('/code', data, function(res) {
 		if (res.success == 1) {
-		  $('.result').html('保存成功\n您的代码地址为: http://52acm.com/' + res.id);
+			$('.result').html('保存成功\n您的代码地址为: http://52acm.com/' + res.id);
 		} else {
-		  $('.result').html('保存失败\n' + res.msg);
+			$('.result').html('保存失败\n' + res.msg);
 		}
 	});
 
@@ -85,7 +85,7 @@ $('#keybingsSelect select').change(function() {
 });
 //设置字体大小
 $('#fontSizeSelect select').change(function() {
-  editor.setFontSize($(this).val());
+	editor.setFontSize($(this).val());
 });
 
 // 设置字体样式
@@ -96,4 +96,68 @@ var $editor = $('#editor');
 $('#fontStyleSelect select').change(function() {
 	setFont($editor, $(this).val());
 });
+
+
 });
+
+
+//聊天室相关的监听代码
+//$(function () {
+	//var CHATSTATE = {isLogin: false};
+	//var content = $('#chat_content');
+	//var status = $('#chat_status');
+	//var input = $('#send_area');
+	//var myColor = false;
+	//var myName = false;
+	//var connection;
+	//function showChat() {
+		//$('#chat_tip').hide();
+		//$('#chat_main').show();
+		//if (CHATSTATE.isLogin == false) {
+			////connection = new WebSocket('ws://sharecode.cloudfoundry.com:3000');
+			//connection = new WebSocket('ws://sharecode.cloudfoundry.com:3001');
+			//connection.onopen = function () {
+				//status.text('选择一个昵称:');
+			//};
+			//connection.onmessage = function (message) {
+				//var json = JSON.parse(message.data);
+				//if (json.type === 'color') { // first response from the server with user's color
+					//myColor = json.data;
+					//status.text(myName + ': ').css('color', myColor);
+				//}else if (json.type === 'message') { // it's a single message
+					//addMessage(json.data.author, json.data.text,
+							//json.data.color, new Date(json.data.time));
+				//} 
+			//};
+			//CHATSTATE.isLogin = true;
+		//}
+	//}
+
+	//function hideChat() {
+		//$('#chat_main').hide();
+		//$('#chat_tip').show();
+	//}
+
+	//$('#chat_tip').click(showChat);
+	//$('#chat_close').click(hideChat);
+
+
+	//input.keydown(function(e) {
+		//if (e.keyCode === 13) {
+			//var msg = $(this).val();
+			//if (!msg)    return;
+			//connection.send(msg);
+			//$(this).val('');
+			//if (myName === false) {
+				//myName = msg;
+			//}
+		//}
+	//});
+
+	//function addMessage(author, message, color, dt) {
+		//content.append('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+				//+ dt.getHours() + ':'
+				//+ (dt.getMinutes()<10?('0'+ dt.getMinutes()):dt.getMinutes()) + '----' + message + '</p>');
+	//}
+//});
+
